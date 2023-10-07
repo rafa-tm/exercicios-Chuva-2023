0bf59939727c3d9164a014412d617b3f4f0e1a2d
Rafael Tury Minatel <rafaelturyminatel@gmail.com>
2023-10-07 00:38:18 -0300

    feat: featuring navigation menu and header

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { NavMenuComponent } from './shared/components/nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ ts/src/app/shared/components/nav-menu/nav-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html',
+})
+export class NavMenuComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
